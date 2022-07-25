d66e76cdf5d4359d6cdadcc0d291985e50206b0c
souvik666 <peacedynasty0@gmail.com>
2022-07-25 14:19:58 +0530

    mongoose connection string issue

--- configs/db.js
+++ configs/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { default: mongoose } = require("mongoose");
 
 const mogno_uri = process.env.mogno_uri || "mongodb://127.0.0.1:27017";
-function Connection() {
+async function Connection() {
   try {
     console.log("Db_Connected");
-    return mongoose.connect(mogno_uri);
+    return await mongoose.connect(mogno_uri);
   } catch (error) {
     console.error("error in /config/db.js " + error);
   }
